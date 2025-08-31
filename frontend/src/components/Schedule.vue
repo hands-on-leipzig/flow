cb19cc29954f14d764b32ba7bd526ec69d328a0f
Thomas Madeya <thomas@madeya.net>
2025-08-31 11:31:44 +0200

    Rework front-end and back-end for first time usage of Ablauf. New entry in plan created if not yet availbe for the event. Then m_paramters is copied to have default values. Preview controller functionality moved into plan controller. Overhaul of routes/aip.php

--- frontend/src/components/Schedule.vue
+++ frontend/src/components/Schedule.vue
@@ -285,16 +285,23 @@ async function getOrCreatePlan() {
   }
 }
 
-const createDefaultPlan = async () => {
   try {
-    const response = await axios.post(`/plans`, {
-      event: selectedEvent?.value?.id,
-      name: 'Zeitplan'
-    })
-    return response.data.id
-  } catch (e) {
-    console.error('Fehler beim Erstellen des Plans', e)
-    return null
+
+    const res = await axios.get(`/plans/event/${selectedEvent?.value?.id}`)
+    const plan = res.data
+    if (!plan || !plan.id) {
+      console.error('Plan konnte nicht erstellt werden oder fehlt')
+      return
+    }
+
+    await axios.get(`/plans/${plan.id}/copy-default`)
+
+    plans.value = [plan]
+    selectedPlanId.value = plan.id
+    await fetchParams(plan.id)
+
+  } catch (err) {
+    console.error('Fehler beim Erstellen oder Initialisieren des Plans:', err)
   }
 }
 
