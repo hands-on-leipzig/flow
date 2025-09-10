459d612c3d31f8e7e8043bc8e1d47b8a27404562
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2025-09-10 17:52:39 +0200

    use admin role from jwt rather than db

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -26,8 +26,7 @@ class User extends Authenticatable
         'subject',
         'password',
         'selection_event',
-        'selection_regional_partner',
-        'is_admin'
+        'selection_regional_partner'
     ];
 
     /**
