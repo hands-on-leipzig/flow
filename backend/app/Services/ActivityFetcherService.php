9301820a5128fcf940a62254699e71e829e5cf8a
Thomas Madeya <thomas@madeya.net>
2025-10-08 08:03:09 +0200

    Fix table names in fetcher

--- backend/app/Services/ActivityFetcherService.php
+++ backend/app/Services/ActivityFetcherService.php
@@ -157,16 +157,16 @@ class ActivityFetcherService
             a.table_1_team as table_1_team,
             a.table_2 as table_2,
             a.table_2_team as table_2_team,
-            CASE a.table_1 
-                WHEN 1 THEN COALESCE(te1.table_name, te3.table_name, "Tisch 1")
-                WHEN 3 THEN COALESCE(te3.table_name, te1.table_name, "Tisch 3")
-                ELSE NULL 
-            END as table_1_name,
-            CASE a.table_2 
-                WHEN 2 THEN COALESCE(te2.table_name, te4.table_name, "Tisch 2")
-                WHEN 4 THEN COALESCE(te4.table_name, te2.table_name, "Tisch 4")
-                ELSE NULL 
-            END as table_2_name
+            CASE a.table_1
+                WHEN 1 THEN COALESCE(te1.table_name, "Tisch 1")
+                WHEN 3 THEN COALESCE(te3.table_name, "Tisch 3")
+                ELSE NULL
+            END AS table_1_name,
+            CASE a.table_2
+                WHEN 2 THEN COALESCE(te2.table_name, "Tisch 2")
+                WHEN 4 THEN COALESCE(te4.table_name, "Tisch 4")
+                ELSE NULL
+            END AS table_2_name
         ';
 
         if ($includeRooms) {
