580619a81c019991fa053e1bcf8c788e6553623e
lucas <ga53foy@mytum.de>
2026-03-01 14:02:12 +0100

    Import behoben

--- backend/app/Http/Controllers/Api/ContaoController.php
+++ backend/app/Http/Controllers/Api/ContaoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use app\Services\ContaoService;
+use App\Services\ContaoService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
--- backend/app/Services/ContaoService.php
+++ backend/app/Services/ContaoService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\Services;
+namespace App\Services;
 
 use Exception;
 use Illuminate\Http\JsonResponse;
